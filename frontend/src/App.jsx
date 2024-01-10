a35bd610351f2d727082f86b4a62bdc0fa7e665b
Johnpaul <indisputable.jay@gmail.com>
2024-01-11 00:22:03 +0100

    over complicated my frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,3 @@
-import { useState } from "react";
-import { Body } from "./Body";
-import { Header } from "./Header";
-import { Footer } from "./Footer";
-
 export default function App() {
-  const [login, setLogin] = useState(false);
-
-  return (
-    <div className="bg-[#F6F4EB] h-screen text-slate-900 grid grid-rows-[auto_1fr_.08fr] ">
-      <Header setLogin={setLogin} />
-      <main className="overflow-scroll">
-        <Body />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <div>Hi</div>;
 }
--- frontend/src/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <div className="bg-[#4682A9] text-center ">
-      <em className="text-[#F6F4EB]">Manage your tasks efficiently</em>
-    </div>
-  );
-}
