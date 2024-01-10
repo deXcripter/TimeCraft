0b8e57d0b16cca00b1ac2566420733079d127b45
Johnpaul <indisputable.jay@gmail.com>
2024-01-10 17:17:54 +0100

    login working

--- frontend/src/Body.jsx
+++ frontend/src/Body.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import axios from "axios";
+import { useState } from "react";
 
 export function Body() {
   return (
@@ -12,28 +13,84 @@ export function Body() {
 }
 
 function Box({ children }) {
-  return (
-    <div className="bg-[#88afc9] h-48 max-w-2xl m-auto p-5">{children}</div>
-  );
+  return <div className="bg-[#88afc9] h-48 w-3/5 m-auto p-5">{children}</div>;
 }
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
   async function handleFormSubmission(e) {
     e.preventDefault();
-    // const stats = await axios.post()
+    console.log("ran");
+
+    try {
+      const stats = await axios.post(
+        "http://localhost:2525/api/v1/users/signup",
+        {
+          password,
+          passwordConfirm,
+          email,
+          name: "hi",
+        }
+      );
+    } catch (err) {
+      console.log(err.response.data.message);
+    }
+  }
+
+  function onChangeEmail(e) {
+    setEmail(e.target.value);
+  }
+  function onChangePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function onChangePasswordConfirm(e) {
+    setPasswordConfirm(e.target.value);
   }
 
   return (
-    <form onSubmit={(e) => handleFormSubmission(e)}>
+    <form onSubmit={(e) => handleFormSubmission(e)} className="grid gap-1.5">
       <p>
-        Your email <input type="email" placeholder="john@gmail.com" />
+        Your email{" "}
+        <input
+          type="email"
+          required
+          placeholder=" john@gmail.com"
+          className="rounded-sm"
+          value={email}
+          onChange={(e) => onChangeEmail(e)}
+        />
       </p>
       <p>
-        Password <input type="password" placeholder="********" />
+        Password{" "}
+        <input
+          type="password"
+          placeholder=" ********"
+          required
+          className="rounded-sm"
+          onChange={onChangePassword}
+          value={password}
+        />
       </p>
       <p>
-        Confirm password <input type="password" placeholder="********" />
+        Confirm password{" "}
+        <input
+          type="password"
+          placeholder=" ********"
+          className="rounded-sm"
+          required
+          onChange={(e) => onChangePasswordConfirm(e)}
+          value={passwordConfirm}
+        />
       </p>
+      <button
+        className="bg-blue-500 w-3/5 mt-5 m-auto rounded-sm"
+        onClick={handleFormSubmission}
+      >
+        Signup
+      </button>
     </form>
   );
 }
