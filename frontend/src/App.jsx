35df08271a200f01f9c2479626ee6089a388de6c
Johnpaul <indisputable.jay@gmail.com>
2024-01-11 17:22:01 +0100

    finished both auth components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,11 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <body className="h-screen text-gray-900 grid grid-rows-[auto_1fr_auto] text-sm">
+    <div className="h-screen text-gray-900  grid grid-rows-[auto_1fr_auto] text-sm">
       <Header/>
       <Body />
       <Footer/>
-    </body>
+    </div>
   );
 }
 
--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,31 +1,96 @@
-export default function Body () {
-  return <section className="bg-blue-100 w-full flex">
-    {/* <Login/> */}
-    <Signup/>
-  </section>
+/* eslint-disable react/prop-types */
+export default function Body() {
+  return (
+    <section className="bg-slate-200 w-full">
+      <AuthBox>
+        <Signup />
+      </AuthBox>
+
+      {/* <Signup/> */}
+    </section>
+  );
 }
 
 function Login() {
+  return (
+    <form className="space-y-6 ">
+      <div>
+        <h1 className="text-xl font-bold">Welcome back!</h1>
+        <p>Log in to access your account</p>
+      </div>
+      <div className="space-y-3">
+        <input
+          placeholder=" Enter your email"
+          type="email"
+          className="rounded-sm h-9 outline-none outline-blue-200 outline-1"
+          required
+        />
 
+        <input
+          placeholder=" Enter your password"
+          type="password"
+          className="rounded-sm h-9 outline-none outline-blue-200 outline-1"
+          required
+        />
 
-  return <form className="bg-blue-500 h-fit w-min px-5 py-2 my-32 mx-auto text-center rounded-md space-y-4 border-b-1 border-l-1 shadow-lg shadow-blue-700 border-blue-300">
-    <div>
-      <h1 className="text-xl">Welcome back!</h1>
-      <p>Log in to access your account</p>
-    </div>
-    <div className="space-y-1">
-
-    <input placeholder=" Enter your email" type="email" className="rounded-sm " required/>
-    <input placeholder=" Enter your password" type="password" className="rounded-sm" required/>
-    <button className="bg-blue-700 rounded-full px-2">submit</button>
-    </div>
-    <p>
-      Not a user? <a href="#">Sign up</a>
-    </p>
-  </form>
+        <button className="bg-blue-500 rounded-sm px-2 w-full text-slate-100 h-9">
+          Login
+        </button>
+      </div>
+      <p className="pb-2">
+        Not a member? <a href="#">Sign up</a>
+      </p>
+    </form>
+  );
 }
 
+function AuthBox({ children }) {
+  return (
+    <div className="bg-slate-50 block h-fit w-min px-5 py-2 my-32 mx-auto text-center rounded-md shadow-lg drop-shadow-lg shadow-slate-600 border-blue-30">
+      {children}
+    </div>
+  );
+}
 
-function Signup () {
-
+function Signup() {
+  return (
+    <form className="space-y-6">
+      <div>
+        <h1 className="text-xl font-bold">Join us today! </h1>
+        <p>Sign up now to become a member</p>
+      </div>
+      <div className="space-y-3">
+        <input
+          type="text"
+          placeholder=" Enter your name"
+          className="rounded-sm h-9 outline-none outline-blue-200 outline-1"
+          required
+        />
+        <input
+          type="email"
+          placeholder=" Enter your email"
+          className="rounded-sm h-9 outline-none outline-blue-200 outline-1"
+          required
+        />
+        <input
+          type="password"
+          placeholder=" Enter your password"
+          className="rounded-sm h-9 outline-none outline-blue-200 outline-1"
+          required
+        />
+        <input
+          type="password"
+          placeholder=" Confirm your password"
+          className="rounded-sm h-9 outline-none outline-blue-200 outline-1"
+          required
+        />
+        <button className="bg-blue-500 rounded-sm px-2 w-full text-slate-100 h-9 ">
+          Signup
+        </button>
+      </div>
+      <p className="pb-2">
+        Already a member? <a href="#">Login</a>
+      </p>
+    </form>
+  );
 }
