e1da56237694a0b804d8bc7ab482f2090fee8872
Johnpaul <indisputable.jay@gmail.com>
2024-01-23 07:22:51 +0100

    ui changes

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -3,7 +3,21 @@ import Login from './Login';
 import { CreateTask } from './CreateTask';
 import { Tasks } from './Tasks';
 import { Controller } from './Controller';
+import { useState } from 'react';
 
 export default function Body() {
-  return <section className=" w-full bg-gray-100 overflow-scroll"></section>;
+  const [loggedInStatus, setLoggedInStatus] = useState(false);
+
+  const triggerLoggedStatus = (bool) => setLoggedInStatus(bool);
+
+  return (
+    <section className=" w-full bg-gray-100 overflow-scroll">
+      {!loggedInStatus && <Login triggerLoggedStatus={triggerLoggedStatus} />}
+      {loggedInStatus && (
+        <h1 className="flex font-bold m-auto text-xl p-40">
+          User is logged in
+        </h1>
+      )}
+    </section>
+  );
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,40 @@
-function Login() {
+import axios from 'axios';
+import { useState } from 'react';
+
+function Login({ triggerLoggedStatus }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        'http://localhost:2525/api/v1/users/signin',
+        {
+          email,
+          password,
+        },
+      );
+
+      const data = await res.data;
+      const token = data.token;
+      localStorage.setItem('token', token);
+      console.log(token);
+      triggerLoggedStatus(true);
+    } catch (err) {
+      console.log(err.message);
+    } finally {
+      setEmail('');
+      setPassword('');
+    }
+    console.log('Signup form submitted');
+  }
+
   return (
-    <form className="max-w-md mx-auto p-8 space-y-6 bg-white rounded-md shadow-md">
+    <form
+      className="max-w-md mx-auto p-8 space-y-6 bg-white rounded-md shadow-md"
+      onSubmit={handleSubmit}
+    >
       <div className="text-center">
         <h1 className="text-2xl font-bold">Welcome back!</h1>
         <p className="text-gray-500">Log in to access your account</p>
@@ -9,6 +43,8 @@ function Login() {
         <input
           placeholder="Enter your email"
           type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           className="rounded-sm h-10 px-4 w-full border border-blue-200 focus:border-blue-400 focus:ring focus:ring-blue-200"
           required
         />
@@ -16,6 +52,8 @@ function Login() {
         <input
           placeholder="Enter your password"
           type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           className="rounded-sm h-10 px-4 w-full border border-blue-200 focus:border-blue-400 focus:ring focus:ring-blue-200"
           required
         />
