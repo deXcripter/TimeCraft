a907695bb8df6f3872b6874b04cdc1ac0a08f0d1
Johnpaul <indisputable.jay@gmail.com>
2024-01-28 15:22:42 +0100

    added logout feature to the frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
   return (
-    <div className="bg-blue-400 flex items-center shadow-blue-600 drop-shadow-sm">
-      <svg className="h-9 w-9"></svg>
-      <span className={styles.logo}>TimeCraft 💡</span>
-    </div>
+    <>
+      <div className="bg-blue-400 flex justify-between shadow-blue-600 drop-shadow-sm">
+        {/* <svg className="h-9 w-9"></svg> */}
+        <span className={styles.logo}>TimeCraft 💡</span>
+        <button className="px-5 uppercase bg-red-500" onClick={() => logout()}>
+          Logout
+        </button>
+      </div>
+    </>
   );
 }
