2ef82845d99f6ab2b6a9e7686d2f7bfec472c631
Johnpaul <indisputable.jay@gmail.com>
2024-02-15 02:19:32 +0100

    improved frontend

--- frontend/prettier.config.cjs
+++
@@ -1,7 +0,0 @@
-// prettier.config.js
-import prettierPluginTailwindcss from "./prettier-plugin-tailwindcss"
-export default  {
-  // plugins: ['prettier-plugin-tailwindcss'],
-  plugins: [prettierPluginTailwindcss],
-  tailwindConfig: './tailwind.config.js'
-}
--- frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-blue-400 flex justify-center h-9 items-center">
-      <p> &copy; TimeCraft - by Johnpaul Nnaji</p>
-    </footer>
-  );
-}
