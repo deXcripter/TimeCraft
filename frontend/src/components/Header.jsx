b22c76f68c8eef29292a1052fb2bdef26523384a
johnpaul <indisputable.jay@gmail.com>
2024-03-07 14:03:48 +0100

    added frontend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+function Header() {
+  return (
+    <div className="header">
+      <Logo />
+      <span className="auth-control">
+        <SignupButton />
+        <LoginButton />
+      </span>
+    </div>
+  );
+}
+
+function Logo() {
+  return <div className="logo">TimeCraft</div>;
+}
+
+function SignupButton() {
+  return <button className="signup">Sign up</button>;
+}
+
+function LoginButton() {
+  return <button className="login">Log in</button>;
+}
+
+export default Header;
