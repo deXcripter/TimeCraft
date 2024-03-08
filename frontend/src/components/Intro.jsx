71e5d24072ee0bacdac7780a89c7ea449f51de1e
johnpaul <indisputable.jay@gmail.com>
2024-03-08 14:59:02 +0100

    good progress

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -1,12 +1,12 @@
 function Intro() {
   return (
     <div className="section-1">
-      <h1>
-        Unlock Productivity <span className="gold-color">Gold</span> with <br />
+      <h1 className="section-1-h1">
+        Unlock Productivity <span className="intro-gold">Gold</span> with <br />
         TimeCraft! Organize, Collaborate, <br />
-        <span className="gold-color"> Conquer Tasks!</span>
+        <span className="intro-gold"> Conquer Tasks!</span>
       </h1>
-      <p>
+      <p className="section-1-p">
         Welcome to TimeCraft, your ultimate destination for seamless task
         managment. From orgainizing your to-dos to collaborating with yout team,
         We have enerything you need to streamline your workflow. Start your
