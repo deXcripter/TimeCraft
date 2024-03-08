7b0fece650eeba198d4722d749d2c7fef8a38b43
johnpaul <indisputable.jay@gmail.com>
2024-03-08 12:17:45 +0100

    setting up sass

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -1,10 +1,16 @@
 function Intro() {
   return (
     <div className="section-1">
-      <p>
+      <h1>
         Unlock Productivity <span className="gold-color">Gold</span> with <br />
         TimeCraft! Organize, Collaborate, <br />
         <span className="gold-color"> Conquer Tasks!</span>
+      </h1>
+      <p>
+        Welcome to TimeCraft, your ultimate destination for seamless task
+        managment. From orgainizing your to-dos to collaborating with yout team,
+        We have enerything you need to streamline your workflow. Start your
+        journery into enhanced productivity today!
       </p>
     </div>
   );
