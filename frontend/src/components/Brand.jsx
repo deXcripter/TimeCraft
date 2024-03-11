f5fab79a37a98791c5f7ccc341eff8976dfbaee0
johnpaul <indisputable.jay@gmail.com>
2024-03-11 01:00:47 +0100

    apps that use timecraft icons

--- frontend/src/components/Brand.jsx
+++ frontend/src/components/Brand.jsx
@@ -3,13 +3,32 @@ function Brand() {
     <div className="brand">
       <h2 className="brand-description">Big Brands that use TimeCraft</h2>
       <ul className="brand-list">
-        <li>Slack</li>
-        <li>Spotify</li>
-        <li>Zelle</li>
-        <li>Adobe</li>
-        <li>Airbab</li>
-        <li>Trivago</li>
-        <li>Barclays</li>
+        <li>
+          <img src="./src/assets/adobe.svg" />
+        </li>
+        <li>
+          {" "}
+          <img src="./src/assets/airbnb.svg" />
+        </li>
+        <li>
+          {" "}
+          <img src="./src/assets/trivago.svg" />
+        </li>
+        <li>
+          {" "}
+          <img src="./src/assets/zelle.svg" />
+        </li>
+        <li>
+          {" "}
+          <img src="./src/assets/barclays.svg" />
+        </li>
+        <li>
+          {" "}
+          <img src="./src/assets/slack.svg" />
+        </li>
+        <li>
+          <img src="./src/assets/saly.svg" />
+        </li>
       </ul>
     </div>
   );
