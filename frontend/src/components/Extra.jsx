c6b841d905a9a898583fe09478157794bf93a14d
johnpaul <indisputable.jay@gmail.com>
2024-03-12 00:57:49 +0100

    celebrating

--- frontend/src/components/Extra.jsx
+++ frontend/src/components/Extra.jsx
@@ -1,5 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
 function Extra() {
-  return <div className="extra"></div>;
+  return (
+    <div className="extra">
+      <section>
+        <img src="./src/assets/trophy.svg" alt="" />
+        <div>
+          <h3>Celebrating 1+ Million Tasks Conquered Worldwide!</h3>
+          <p>
+            Thanks to our dedicated users, TimeCraft has empowered individuals
+            and teams across the globe to streamline their workflow, enhance
+            productivity, and achieve remarkable milestones. Together, we're
+            redefining the way tasks are managed, one accomplishment at a time."
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Extra;
