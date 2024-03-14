1c4a9652227492b733cefc1d03c4e06dba2c44af
johnpaul <indisputable.jay@gmail.com>
2024-03-14 15:54:08 +0100

    messed up my logo component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,29 @@
 function Footer() {
   return (
     <div className="footer">
-      <div className="homepage-logo">
+      <div className="footer-logo-img">
         <img src="./src/assets/saly.svg" />
-        <p className="homepage-logo-text">TimeCraft.</p>
+        <p className="footer-logo-img">TimeCraft.</p>
       </div>
+      <nav>
+        <div className="footer-section">
+          <h3>Quicklinks</h3>
+          <li>Landing page</li>
+          <li>Testimonials</li>
+        </div>
+        <div className="footer-section">
+          <h3>Explore</h3>
+          <ul>
+            <li>TimeCraft Mobile</li>
+            <li>Plannit|</li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h3>Contact</h3>
+          <li>Awka, Anambara</li>
+          <li>support@timecraft.com</li>
+        </div>
+      </nav>
 
       <hr />
       <div className="copyright">
