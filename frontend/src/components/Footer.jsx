947b05a655bdf0b7605bfc96894c2e2f5d0bc668
johnpaul <indisputable.jay@gmail.com>
2024-03-14 15:32:24 +0100

    removing abstraction

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import { Logo } from "./Logo";
-
 function Footer() {
   return (
     <div className="footer">
-      <Logo />
+      <div className="homepage-logo">
+        <img src="./src/assets/saly.svg" />
+        <p className="homepage-logo-text">TimeCraft.</p>
+      </div>
+
+      <hr />
+      <div className="copyright">
+        All right reserved. Pivacy policy | Terms of Service | Contact Us
+      </div>
     </div>
   );
 }
--- frontend/src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-export function Logo() {
-  return (
-    <div className="homepage-logo">
-      <img src="./src/assets/saly.svg" />
-      <p className="homepage-logo-text">TimeCraft.</p>
-    </div>
-  );
-}
