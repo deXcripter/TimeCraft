5349856d8d6b441718ff7a867092972670d82c88
johnpaul <indisputable.jay@gmail.com>
2024-03-14 19:09:42 +0100

    finishing the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,35 +1,36 @@
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer-logo-img">
-        <img src="./src/assets/saly.svg" />
-        <p className="footer-logo-img">TimeCraft.</p>
+    <>
+      <div className="footer">
+        <div className="footer-logo-img">
+          <img src="./src/assets/saly.svg" />
+          <p className="footer-logo-img">TimeCraft.</p>
+        </div>
+        <nav>
+          <div className="footer-section">
+            <h3>Quicklinks</h3>
+            <li>Landing page</li>
+            <li>Testimonials</li>
+          </div>
+          <div className="footer-section">
+            <h3>Explore</h3>
+            <ul>
+              <li>TimeCraft Mobile</li>
+              <li>Plannit</li>
+            </ul>
+          </div>
+          <div className="footer-section">
+            <h3>Contact</h3>
+            <li>Awka, Anambara</li>
+            <li>support@timecraft.com</li>
+          </div>
+        </nav>
       </div>
-      <nav>
-        <div className="footer-section">
-          <h3>Quicklinks</h3>
-          <li>Landing page</li>
-          <li>Testimonials</li>
-        </div>
-        <div className="footer-section">
-          <h3>Explore</h3>
-          <ul>
-            <li>TimeCraft Mobile</li>
-            <li>Plannit|</li>
-          </ul>
-        </div>
-        <div className="footer-section">
-          <h3>Contact</h3>
-          <li>Awka, Anambara</li>
-          <li>support@timecraft.com</li>
-        </div>
-      </nav>
-
       <hr />
       <div className="copyright">
         All right reserved. Pivacy policy | Terms of Service | Contact Us
       </div>
-    </div>
+    </>
   );
 }
 
